2d89abf30bccf92e3348aecd126475939794c6c3
Arm <1364527147@qq.com>
2020-08-17 18:18:33 +0800

    解决跨域问题

--- src/main/java/com/dhu/test4service/controller/UserController.java
+++ src/main/java/com/dhu/test4service/controller/UserController.java
@@ -3,11 +3,9 @@ package com.dhu.test4service.controller;
 import com.dhu.test4service.pojo.User;
 import com.dhu.test4service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -15,35 +13,22 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping(value = "/register")
-    public User register(@RequestParam("name") String name, @RequestParam("password") String password, @RequestParam("apartment") Integer apartment, @RequestParam("role") Integer role)
+    @GetMapping(value = "/login")
+    public Boolean login(@RequestParam("name") String name, @RequestParam("password") String password)
     {
-        User user1=new User(name,password,apartment,role);
+        return userService.findByNameAndPassword(name,password);
+    }
+
+    @GetMapping(value = "/register")
+    public User register(@RequestParam("name") String name, @RequestParam("password") String password)
+    {
+        User user1=new User(name,password,1,1);
         return userService.save(user1);
     }
 
     @GetMapping("/role")
-    public String getRole(@RequestParam("name") String name){
+    public Integer getRole(@RequestParam("name") String name){
         Integer op=userService.getRole(name);
-        switch (op)
-        {
-            case -1:
-            {
-                return "fail";
-            }
-            case 1:
-            {
-                return "role1";
-            }
-            case 2:
-            {
-                return "role2";
-            }
-            case 3:
-            {
-                return "role3";
-            }
-        }
-        return "fail";
+        return op;
     }
 }
