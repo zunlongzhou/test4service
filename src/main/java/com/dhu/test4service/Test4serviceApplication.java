cb26f0fb01a66fd51325007c4f39ff05829f781a
yesusan <1245012106@qq.com>
2020-11-03 14:10:13 +0800

    tomcat打war包

--- src/main/java/com/dhu/test4service/Test4serviceApplication.java
+++ src/main/java/com/dhu/test4service/Test4serviceApplication.java
@@ -2,9 +2,16 @@ package com.dhu.test4service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Test4serviceApplication {
+public class Test4serviceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder){
+        return springApplicationBuilder.sources(Test4serviceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Test4serviceApplication.class, args);
